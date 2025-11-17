1ff5886026c68e9e17e1e6cb42d2cd8dd5cc2a6d
Jack Berg <johnmberg8@gmail.com>
2025-11-17 15:05:44 -0600

    yaml-to-json annotates , , and replaces .yaml refs with .json

--- scripts/yaml-to-json.js
+++ scripts/yaml-to-json.js
@@ -6,10 +6,31 @@ import yaml from "yaml";
 fs.rmSync(schemaOutDirPath, {recursive: true, force: true});
 fs.mkdirSync(schemaOutDirPath);
 
-// Parse YAML schema source files and write as JSON to schema out directory
+// Read YAML source files and create a map of their content
+const fileContentByFile = {};
 fs.readdirSync(schemaSourceDirPath)
     .filter(file => file.endsWith('.yaml') && !file.startsWith(metaSchemaFilePrefix))
-    .forEach(file => {
-        const parsedContent = yaml.parse(fs.readFileSync(schemaSourceDirPath + file, "utf-8"));
-        fs.writeFileSync(schemaOutDirPath + file.replace('.yaml', '.json'), JSON.stringify(parsedContent, null, 2));
+    .forEach(file => fileContentByFile[file] = fs.readFileSync(schemaSourceDirPath + file, "utf-8"));
+
+// Iterate through source files, replacing references
+Object.keys(fileContentByFile).forEach(file => {
+    const jsonFile = file.replace('.yaml', '.json');
+    Object.entries(fileContentByFile).forEach(([otherFile, otherContent]) => {
+        fileContentByFile[otherFile] = otherContent.replaceAll(`$ref: ${file}`, `$ref: ${jsonFile}`);
     });
+});
+
+// For each file, parse the YAML, annotate, and write to output directory
+Object.entries(fileContentByFile).forEach(([file, content]) => {
+    const parsedContent = yaml.parse(content);
+    const jsonFile = file.replace('.yaml', '.json');
+
+    // Annotate with constant info
+    const annotated = {
+        '$id': `https://opentelemetry.io/otelconfig/${jsonFile}`,
+        '$schema': 'https://json-schema.org/draft/2020-12/schema',
+        ...parsedContent
+    }
+
+    fs.writeFileSync(schemaOutDirPath + jsonFile, JSON.stringify(annotated, null, 2));
+});
